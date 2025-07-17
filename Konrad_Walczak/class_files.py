f678c2dbe88c022c90dc82baf83823a186d3eef6
congiu-w <220693236+congiu-w@users.noreply.github.com>
2025-07-17 11:21:48 +0200

    reworked from scratch the whole file class_files.py
    every function has been rewritten, comments and docstrings are planned to be added at a later time

--- Konrad_Walczak/class_files.py
+++ Konrad_Walczak/class_files.py
@@ -2,113 +2,150 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats 
 
-def polar_to_cartesian(r, phi):
-    '''
-    Function transforming polar coordinates (r, phi) to cartesian (x,y). 
-    '''
-    return (r*np.sin(phi), r*np.cos(phi))
+def empty_line():
+    print(20*"*")
 
-class Single_Particle_Tracking():
+class Single_Particle():
+    def __init__(self, name = "Particle_", layers = 9, theta0_min = 0, theta0_max = np.pi, detector_angle = np.pi/4, pt = 10):
+        #initiation conditions
+        self.theta0_min = theta0_min
+        self.theta0_max = theta0_max
 
-    def __init__(self, name = "DEFAULT", layers = 9):
-        '''
-        Constructor function for class Single_Particle_Tracking, particle can be given a custom name via |name| (to be used later 
-        for massive MC simulations), amount of layers of detectors can be specified via |layers|. 
-
-        Variables in function are:
-        self.name - type: string, name of a particle 
-        self.q - type: int, randomly assigned electric charge of a particle
-        self.layers - type: int, amount of detectors where particle's position is measured
-        self.params - type: int, parameters influencing particle's behaviour (mass, velocity, magnetic field etc.) #TODO: currently assumed to be of unity
-        self.theta0 - type: float, initial angle of emission of a particle, randomly generated value from an interval of (0, PI/4)
-        self.r - type: np.array [1, 2, 3 ... 9] of len(layers), measurements of particle's radial position in polar coordinates at equally spaced detectors
-        self.phi - type: np.array of len(layers) (by default set to None), measurments of particle's angular position in polar coordinated at equally spaced detectors
-        
-        Additional comment:
-        Currently it is assumed that we work in 2D cross-section of a detector and particles can only travel in XY dimensions.
-        #TODO: if enough time is left then we might expand our model for 3D paths
-        
-        '''
-
-        self.name = str(name)
-        self.q = np.random.choice([-1, 1]) 
+        #detector's properties
+        self.pT = pt
         self.layers = int(layers)
-        self.params = 1 #TODO
-        self.theta0 = np.random.uniform(0, np.pi/4)
+        self.A = 1
+        self.detector_angle = detector_angle
+
+        #particle's properties
+        self.name = name
+        self.q = np.random.choice([-1,1])
+        self.theta0 = np.random.uniform(theta0_min + 0.05 * (theta0_max-theta0_min), theta0_max - (theta0_max-theta0_min))
         self.r = np.linspace(1, layers, layers)
         self.phi = None
+        self.maximal_curvature = self.q * self.A / self.pT
 
-    def generate_path(self, noise_level = 0, max_divisor = 16):
-        '''
-        Function used to randomize particle's path in the detector. Amount of noise at the detector can be specified via |noise_level|,
-        whereas |max_divisor| is used for generation of a deterministic particle's path. This function currently modifies the values of self.phi.
-
-        Firstly, a number from uniform distribution from interval (4, max_divisor) is scaled via self.params, this is angular_divisor.
-        Secondly, a np.array of len(self.layers) of equally spaced angles between (0, PI/angular_divisor) and np.array of measurement_noise 
-        (from a normal distribution of mean = 0, scale = noise_level; noise is assumed to be of a normal distribution as stated in Chapter 4.2 of [1]) are created.
-        Lastly, depending on a particle's charge, both are either added or subtracted from the particle's initial angle, thus obtaining a path in the detector.
-
-        Additional comment:
-        Currently I believe this function is sufficient enough to generate simplified circular paths. However, this shall not remain unmodified and should be revised soon.
-        '''
-
+        #
+        self.detection_points_x, self.detection_points_y, self.curvature = None, None, None
         
-        angular_divisor = self.params * scipy.stats.uniform.rvs(4, max_divisor)
-        dphi = np.linspace(0, np.pi/angular_divisor, self.layers)
-        measurement_noise = scipy.stats.norm.rvs(loc = noise_level, size = self.layers)
 
-        if self.q>0:
-            self.phi = self.theta0 + dphi + measurement_noisenoise
-        else:
-            self.phi = self.theta0 - dphi + measurement_noisenoise
+    def generate_path(self, measurement_error = 0):
+        curvature = np.random.uniform(0, self.maximal_curvature)
+        detection_points_x, detection_points_y = np.zeros(shape = self.layers), np.zeros(shape = self.layers)
+        thetas = self.theta0 + scipy.stats.norm.rvs(loc = 0, scale = measurement_error) + self.r * curvature
+        detection_points_x = self.r * np.cos(thetas)
+        detection_points_y = self.r * np.sin(thetas)
+        mask = np.abs(thetas)<= self.detector_angle
+        #mask = True
+        self.detection_points_x, self.detection_points_y, self.curvature = detection_points_x[mask], detection_points_y[mask], curvature
+    
+    def plot_path(self, measurement_error = 0):
+        self.generate_path(measurement_error)
+        detection_points_x, detection_points_y, curvature = self.detection_points_x, self.detection_points_y, self.curvature
+        angles = np.linspace(-np.pi/2, np.pi/2, 1000)
 
-    def plot_path(self):
-        '''
-        Simple function to plot particle's path using (r, phi) in polar coordinates.
-        #TODO: redo the style of a plot.
+        plt.figure(figsize= (10, 10))
+        plt.axhline(color = 'black', linewidth = 0.5)
+        for radius in self.r:
+            x_circle = radius * np.cos(angles)
+            y_circle = radius * np.sin(angles)
+            plt.plot(x_circle, y_circle, color = "black", linewidth = 0.5)
+        plt.plot(detection_points_x, detection_points_y, color = "red", marker = 'o', linestyle = 'dashed', label = {self.name})
         
-        '''
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='polar')
-        c = ax.scatter(self.phi, self.r, marker = '.', color = 'red', label = f"{self.name}")
-        plt.title("Path")
-        plt.legend()
-        #plt.show()
-        plt.savefig("simulation_" + self.name)
-
-    def hough_transform(self):
-        '''
-        Function responsible for Hough transform of a particle's path.
-
-        Addition comment:
-        My current understanding of Hough transform is not sufficient to elaborate any further on how it works.
-
-        #TODO: work out what exactly is Hough transform, ask the supervisor and rework the function.
-        '''
-        x, y = polar_to_cartesian(self.r, self.phi)
-        x_axis = np.linspace(0, np.pi, 1000)
-        y_axis = []
-        for i in range(self.layers):
-            curve = 2/self.r[i] * np.sin(x_axis + self.phi[i])
-            y_axis.append(curve)
-        return x_axis, y_axis
-
-    def plot_hough_transform(self):
-        '''
-        Simple function to plot Hough transform of a particle's path obtained from hough_transform(self). It remains adequate
-        with data format used in an aforementioned function.
-
-        #TODO: redo the function upon modifications of hough_transform()
-        '''
-        x, y_curves = self.hough_transform()
-        plt.figure()
-        for i in range(self.layers):
-            plt.plot(x, y_curves[i], label = f"", linestyle = '-')
-        plt.title("Hough space")
+        
         plt.legend()
         plt.show()
+
+
+    def perform_hough_transform(self, hough_space_bins=1024):
+
+        theta_range = np.linspace(self.theta0_min, self.theta0_max, hough_space_bins)
+        curvature_range = np.linspace(-self.maximal_curvature, self.maximal_curvature, hough_space_bins)
+
         
-'''
-BIBLIOGRAPHY:
-[1]: Rudolf Frühwirth Are Strandlie "Pattern Recognition, Tracking and Vertex Reconstruction in Particle Detectors"
-'''
+        plt.figure(figsize=(8, 8))
+        for x, y in zip(self.detection_points_x, self.detection_points_y):    
+            curvature_line = np.sin((np.arctan2(y, x) - theta_range)) / np.sqrt(x**2 + y**2)
+            
+            
+            #curvature_line = np.clip(curvature_line, curvature_range[0], curvature_range[-1])
+            plt.plot(theta_range, curvature_line, alpha=0.6, color = 'black')
+
+        plt.xlabel("Theta₀ (rad)")
+        plt.ylabel("Curvature")
+        plt.title(f"Hough Space Lines for: {self.name}")
+        plt.grid(True)
+        plt.axhline(0, color='black', lw=0.5)
+        plt.axvline(0, color='black', lw=0.5)
+        plt.show()
+
+    def hough_transform_data(self, hough_space_bins = 1024):
+        theta_range = np.linspace(self.theta0_min, self.theta0_max, hough_space_bins)
+        curvature_range = np.linspace(-self.maximal_curvature, self.maximal_curvature, hough_space_bins)
+        curvature_lines = []
+
+        for x, y in zip(self.detection_points_x, self.detection_points_y):    
+            curvature_lines.append(np.sin((np.arctan2(y, x) - theta_range)) / np.sqrt(x**2 + y**2))
+        
+        return theta_range, curvature_lines
+
+class Simulation():
+    def __init__(self, n = 5, detector_angle = np.pi/8):
+        empty_line()
+        print("INITIATING SIMULATION...")
+
+
+        self.count = n
+        self.particles = []
+        self.detector_angle = detector_angle
+        for i in range(n):
+            pt = np.random.uniform(0, 100)
+            measurement_error = np.random.uniform(0, np.pi/100)
+
+            particle = Single_Particle(name = i, pt = pt, detector_angle = self.detector_angle)
+
+            particle.generate_path(measurement_error)
+            self.particles.append(particle)
+            
+    def plot_paths(self):
+        empty_line()
+        print("PLOTTING PATHS...")
+        fig, ax = plt.subplots(figsize= (10, 10))
+        angles = np.linspace(-np.pi/2, np.pi/2, 1000)
+        plt.axhline(color = 'black', linewidth = 0.5)
+
+        for radius in self.particles[0].r:
+            x_circle = radius * np.cos(angles)
+            y_circle = radius * np.sin(angles)
+            ax.plot(x_circle, y_circle, color = "black", linewidth = 0.5)
+        for i in range(self.count):
+            #ax.plot(self.particles[i].detection_points_x, self.particles[i].detection_points_y, marker = 'o', linestyle = 'dashed', label = {self.particles[i].name})
+            ax.plot(self.particles[i].detection_points_x, self.particles[i].detection_points_y, marker = 'o', linestyle = 'dashed')
+        
+        max_r = self.particles[0].r[-1] + 1  
+        for angle in [self.detector_angle, -self.detector_angle]:
+            x_line = [0, max_r * np.cos(angle)]
+            y_line = [0, max_r * np.sin(angle)]
+            plt.plot(x_line, y_line, color="black", linestyle="--", linewidth=5, label = f"Limit of the detector at {np.rad2deg(angle):.1f} degrees")
+        
+
+        plt.legend()
+        plt.show()
+
+
+    def hough_transform_lines(self):
+
+        plt.figure(figsize=(8, 8))
+
+        for particle in self.particles:
+            theta_range, curvature_lines = particle.hough_transform_data()
+            for curve in curvature_lines:
+                plt.plot(theta_range, curve, alpha=0.6, color = 'black')
+        
+        plt.xlabel("Theta₀ (rad)")
+        plt.ylabel("Curvature")
+        plt.grid(True)
+        plt.axhline(0, color='black', lw=0.5)
+        plt.axvline(0, color='black', lw=0.5)
+        plt.show()
+
